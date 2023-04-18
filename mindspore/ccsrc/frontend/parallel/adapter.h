17bd46b1ee1aed7a7d3d7b6f29d83682fade28c9
zhenghui <zhenghui@hdu.edu.cn>
2023-04-18 19:09:49 +0800

    更新 'mindspore/ccsrc/frontend/parallel/adapter.h'
    v2

--- mindspore/ccsrc/frontend/parallel/adapter.h
+++ mindspore/ccsrc/frontend/parallel/adapter.h
@@ -1,6 +1,3 @@
-#include <cinttypes>
-#include <ctime>
-#include <algorithm>
 #include <map>
 #include <memory>
 #include <set>
@@ -17,40 +14,34 @@ using namespace framework;
 class adapter {
 
     public:
-	framework::Graph hdu_graph;
-	std::map<std::string, std::vector<std::vector<std::int64_t> >> strategy;
-	std::map<std::string, std::int64_t> op_stage_id;
+        framework::Graph hdu_graph;
+        std::map<std::string, std::vector<std::vector<std::int64_t> >> strategy;
+        std::map<std::string, std::string> op_node;
 
-	void ConvertMSGraphToHDUGraph(framework::NodeBase hdu_node){
-        	this->hdu_graph.AddNode(hdu_node);
-    	}
-    	void ConvertMSNodeToHDUNode(const std::string node_name, const std::string op,
+        void ConvertMSGraphToHDUGraph(framework::NodeBase hdu_node){
+                this->hdu_graph.AddNode(hdu_node);
+        }
+        void ConvertMSNodeToHDUNode(const std::string node_name, const std::string op,
                             const std::string device, const std::vector<std::string> inputs,
                             const std::map<std::string, std::string> attrs) {
-        	framework::NodeBase hdu_node;
-        	hdu_node.Device(device);
-        	hdu_node.Name(node_name);
-        	hdu_node.Op(op);
-        	hdu_node.Inputs(inputs);
-        	hdu_node.Attrs(attrs);
-        	ConvertMSGraphToHDUGraph(hdu_node);
-    	}
-
-	std::vector<std::vector<std::int64_t> > GetHDUStrategy(string op) {
-                return this->strategy[op];
+                framework::NodeBase hdu_node;
+                hdu_node.Device(device);
+                hdu_node.Name(node_name);
+                hdu_node.Op(op);
+                hdu_node.Inputs(inputs);
+                hdu_node.Attrs(attrs);
+                ConvertMSGraphToHDUGraph(hdu_node);
+                op_node.insert(std::pair<std::string, std::string>(op, node_name));
         }
 
-	void SetHDUStrategy(std::string op, std::vector<std::vector<std::int64_t> > stra) {
-                this->strategy.insert(std::pair<std::string, std::vector<std::vector<std::int64_t> >>(op, stra));
-
+        std::vector<std::vector<std::int64_t> > GetHDUStrategy(string node) {
+                return this->strategy[node];
         }
 
-	std::int64_t GetStageId(std::string op) {
-                return this->op_stage_id[op];
+        void SetHDUStrategy(std::string node, std::vector<std::vector<std::int64_t> >stra) {
+              this->strategy.insert(std::pair<std::string, std::vector<std::vector<std::int64_t> >>(node, stra));
 
         }
 
-        void SetStageId(std::string op, std::int64_t stageId) {
-                this->op_stage_id.insert(std::pair<std::string, std::int64_t>(op, stageId));
-        }
+
 };
